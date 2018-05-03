030462d053dffaa586af6f1c715d1d856913cdac
emityakov <iyao@mail.ru>
2018-05-03 21:19:10 +0400

    Chapter 4.5

---
+++ RecursivePictures/app/src/main/java/ru/samsung/itschool/book/fractal/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.samsung.itschool.book.fractal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyDraw(this));
+    }
+}
---
+++ RecursivePictures/app/src/main/java/ru/samsung/itschool/book/fractal/MyDraw.java
@@ -0,0 +1,13 @@
+package ru.samsung.itschool.book.fractal;
+
+import android.view.View;
+
+/**
+ * Created by Компьютер on 03.05.2018.
+ */
+
+class MyDraw extends View {
+    public MyDraw(MainActivity mainActivity) {
+        super();
+    }
+}
