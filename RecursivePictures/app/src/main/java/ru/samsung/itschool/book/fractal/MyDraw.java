8af1fd19584a8676e05a590c57bbb5e66d441988
emityakov <iyao@mail.ru>
2018-05-04 00:12:00 +0400

    Chapter 4.5

--- RecursivePictures/app/src/main/java/ru/samsung/itschool/book/fractal/MyDraw.java
+++ RecursivePictures/app/src/main/java/ru/samsung/itschool/book/fractal/MyDraw.java
@@ -1,13 +1,46 @@
 package ru.samsung.itschool.book.fractal;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
-/**
- * Created by Компьютер on 03.05.2018.
- */
 
-class MyDraw extends View {
-    public MyDraw(MainActivity mainActivity) {
-        super();
+public class MyDraw extends View {
+    Paint paint = new Paint();
+    int w,h;
+
+    public MyDraw(Context context) {
+        super(context);
+
+        paint = new Paint();
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(3);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        drawCircles(canvas, w/2, h/2, 200);
+    }
+
+    public void drawCircles(Canvas canvas, int x, int y, int r){
+        canvas.drawCircle(x, y, r, paint);
+        if(r > 10) {
+            drawCircles(canvas, x, y - r, r / 2);
+            drawCircles(canvas, x + r, y, r / 2);
+            drawCircles(canvas, x, y + r, r / 2);
+            drawCircles(canvas, x - r, y, r / 2);
+        }
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        this.w = w;
+        this.h = h;
     }
 }
+
